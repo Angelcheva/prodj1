c9e07593fa49fa5e51d7ca376c94610cf8f76149
Angelcheva <angelcheva_elena@bk.ru>
2019-01-29 14:41:20 +0300

    Занятие 2
    Знакомство с : выражениями, опирациями,  перемеными, значениями, функциями, классами и обьектами

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -2,6 +2,23 @@ public class MyFirstProgram {
 
   public static void main (String[] args) {
 	System.out.print("Hello,world!\n\n");
-	System.out.print("My name is Lena");
+	System.out.println("My name is Lena");
+      System.out.println(2+2);
+      System.out.println(2*2);
+      System.out.println(2-2);
+      System.out.println(2/2);
+      System.out.println(1.0/2);
+      System.out.println((2+2)*2);
+      System.out.println(("2"+"2"));
+      System.out.println("2"+2);
+      System.out.println("2+2=" +(2+2));
+      Square square= new Square(2, 3);
+      square.a=2;
+      square.b=3;
+      Integer integer= setName(square.a, square.b);
+      System.out.println("sun="+ integer);
+  }
+  private static Integer setName(int a, int b){
+      return a+b;
   }
 }
---
+++ src/main/java/Square.java
@@ -0,0 +1,9 @@
+public class Square {
+    int a = 2;
+    int b = 3;
+
+    public Square(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
